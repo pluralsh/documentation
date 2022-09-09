c0ec483149456643c02745f12a5c0f97974ace36
Klink <85062+dogmar@users.noreply.github.com>
2022-09-09 15:23:49 -0700

    Enable video embeds, including from loom

---
+++ markdoc/tags/embed.markdoc.ts
@@ -0,0 +1,20 @@
+import Embed from '../../components/Embed'
+
+
+export const embed = {
+  render: Embed,
+  description: "Render embeddable content using react-embed",
+  attributes: {
+    url: { type: String, required: true, description: "URL to display." },
+    isDark: {
+      type: Boolean,
+      description:
+        "True if dark mode enable. In that case will try to render content on dark background.",
+    },
+    width: {
+      type: Number,
+      description:
+        "Number of pixels the maximum space available to the component. If not provided defaults to window width.",
+    },
+  },
+};
