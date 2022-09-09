d1990f1a355a6dfa91c24e107e94edcd9227889d
Klink <85062+dogmar@users.noreply.github.com>
2022-09-09 15:26:07 -0700

    temp commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withMarkdoc = require('@markdoc/next.js');
+const withMarkdoc = require("@markdoc/next.js");
 
-module.exports =
-  withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-  });
+module.exports = withMarkdoc(
+  /* config: https://markdoc.io/docs/nextjs#options */ {
+    schemaPath: "./markdoc",
+  }
+)({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
+  async redirects() {
+    return [
+      { source: "/", destination: "/getting-started/readme", permanent: false },
+    ];
+  },
+});
+``;
