dd19c498d5df9d6970f8113bcc0cacc8b09e8866
Klink <85062+dogmar@users.noreply.github.com>
2022-09-12 16:52:05 -0700

    Very basic theming enabled
    Still crashes when adding theme to honorable them provider

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,34 +1,8 @@
-import Prism from 'prismjs'
-
 import * as React from 'react'
-import styled from 'styled-components'
+import { Code } from 'pluralsh-design-system'
 
-export const CodeBlock = styled(({ children, language }) => {
+export const CodeBlock = ({ children, language }) => {
   const ref = React.useRef(null)
 
-  React.useEffect(() => {
-    if (ref.current) Prism.highlightElement(ref.current, false)
-  }, [children])
-
-  return (
-    <div
-      className="code"
-      aria-live="polite"
-    >
-      <pre
-        ref={ref}
-        className={`language-${language}`}
-      >
-        {children}
-      </pre>
-    </div>
-  )
-})`
-  position: relative;
-
-  /* Override Prism styles */
-  & :global(pre[class*='language-']) {
-    text-shadow: none;
-    border-radius: 4px;
-  }
-`
+  return <Code language={language}>{children}</Code>
+}
