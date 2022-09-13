7d0c6ef41bb7a5230545cc2a59aa9b23b31a24f1
Klink <85062+dogmar@users.noreply.github.com>
2022-09-12 17:52:52 -0700

    Slightly style topnav

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -2,34 +2,30 @@ import React from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 
-export const TopNav = styled(({ children, ...props }) => (
-  <nav {...props}>
-    <Link
-      href="/"
-      className="flex"
-    >
-      Home
-    </Link>
-    <section>{children}</section>
-  </nav>
-))`
-    top: 0;
-    position: fixed;
-    width: 100%;
-    z-index: 100;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
-    padding: 1rem 2rem;
-    background: white;
-    border-bottom: 1px solid var(--border-color);
-  & :global(a) {
-    text-decoration: none;
-  }
-  section {
-    display: flex;
-    gap: 1rem;
-    padding: 0;
-  }
-`
+export const TopNav = styled(({ children, ...props }) => {
+  return (
+    <nav {...props}>
+      <Link href="/" className="flex">
+        Home
+      </Link>
+      <section>{children}</section>
+    </nav>
+  )
+})(({ theme }) => ({
+  top: 0,
+  position: 'fixed',
+  height: 'var(--top-nav-height)',
+  width: '100%',
+  zIndex: '100',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: '1rem',
+  padding: '1rem 2rem',
+  borderBottom: `1px solid ${theme.colors.border}`,
+  section: {
+    display: 'flex',
+    gap: '1rem',
+    padding: '0',
+  },
+}))
