d2658eb1ba1d48047e1d809d4ae7232b550ca203
Klink <85062+dogmar@users.noreply.github.com>
2022-09-15 12:20:42 -0700

    try to get emotion and styled-components working better with SSR

--- next.config.js
+++ next.config.js
@@ -1,44 +1,44 @@
-const withPlugins = require('next-compose-plugins')
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line function-paren-newline
 const withMarkdoc = require('@markdoc/next.js')(
   /* config: https://markdoc.io/docs/nextjs#options */ {
     schemaPath: './markdoc',
-  }
-)
-const withTM = require('next-transpile-modules')(
-  ['pluralsh-design-system', 'honorable', 'honorable-theme-default'],
+  })
+const withTM = require('next-transpile-modules')(['pluralsh-design-system', 'honorable', 'honorable-theme-default'],
   {
     debug: false,
-  }
-)
+  })
 
 module.exports = () => {
   const plugins = [withMarkdoc, withTM]
-  return plugins.reduce(
-    (acc, next) => {
-      return next(acc)
+
+  return plugins.reduce((acc, next) => next(acc), {
+    reactStrictMode: true,
+    compiler: {
+      // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+      emotion: true,
+      styledComponents: true,
     },
-    {
-      i18n: {
-        locales: ['en-US'],
-        defaultLocale: 'en-US',
-      },
-      pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
-      async redirects() {
-        return [
-          {
-            source: '/getting-started/readme',
-            destination: '/',
-            permanent: false,
-          },
-        ]
-      },
-      // webpack: (config) => {
-      //   config.module.rules.push({
-      //     test: /\.md$/,
-      //     use: 'raw-loader',
-      //   });
-      //   return config;
-      // },
-    }
-  )
+    i18n: {
+      locales: ['en-US'],
+      defaultLocale: 'en-US',
+    },
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
+    async redirects() {
+      return [
+        {
+          source: '/getting-started/readme',
+          destination: '/',
+          permanent: false,
+        },
+      ]
+    },
+    // webpack: (config) => {
+    //   config.module.rules.push({
+    //     test: /\.md$/,
+    //     use: 'raw-loader',
+    //   });
+    //   return config;
+    // },
+  })
 }
