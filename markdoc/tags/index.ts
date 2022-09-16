3063cd24c8eaa0557ebd4b3e1ee240ba961bfe17
Klink <85062+dogmar@users.noreply.github.com>
2022-09-15 17:45:47 -0700

    Add Tabs

--- markdoc/tags/index.ts
+++ markdoc/tags/index.ts
@@ -1,5 +1,6 @@
 /* Use this file to export your markdoc tags */
 export * from './callout.markdoc'
 export * from './embed.markdoc'
+export * from './tabs.markdoc'
 // export { comment, head, script } from '@markdoc/next.js/tags'
 export * from '../nodes/link.markdoc'
---
+++ markdoc/tags/tabs.markdoc.ts
@@ -0,0 +1,32 @@
+import { Tag } from '@markdoc/markdoc'
+
+import { Tab, Tabs } from '../../components/Tabs'
+
+export const tabs = {
+  render: Tabs,
+  description: 'Display horizontal tabs in a box',
+  children: ['tab'],
+  attributes: {},
+  transform(node, config) {
+    console.log('tab node', node)
+    const titles = node
+      .transformChildren(config)
+      .filter(child => child && child.name === 'Tab')
+      .map(tab => (typeof tab === 'object' ? tab.attributes.title : null))
+
+    return new Tag(this.render as any,
+      { titles },
+      node.transformChildren(config))
+  },
+}
+
+export const tab = {
+  render: Tab,
+  description: 'Display content in a tab',
+  attributes: {
+    title: {
+      type: String,
+      description: 'The title displayed on the tab',
+    },
+  },
+}
