0ce911847b6e24eb01acecaeca4dbfccd9f0b0cf
Klink <85062+dogmar@users.noreply.github.com>
2022-09-19 17:54:22 -0700

    Fix SSR for styled-components
    And fix hydration error in SideNav.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -118,10 +118,7 @@ function MyApp({ Component, pageProps }: AppPropsPlusMd) {
               href="https://fonts.gstatic.com"
               crossOrigin=""
             />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
-              rel="stylesheet"
-            />
+
           </Head>
           <TopNav>{/* <Link href="/docs">Docs</Link> */}</TopNav>
           <Page>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,55 @@
 // pages/_document.js
-import {
-  Head, Html, Main, NextScript,
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
 } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link
-          rel="preconnect"
-          href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}-dsn.algolia.net`}
-          crossOrigin=""
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      })
+
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    }
+    finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="preconnect"
+            href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}-dsn.algolia.net`}
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
