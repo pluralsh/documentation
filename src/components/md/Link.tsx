4aa38157e1312c3255db649ab1ac615625dff529
Klink <85062+dogmar@users.noreply.github.com>
2022-09-26 15:05:34 -0700

    Fix links using relative paths

--- src/components/md/Link.tsx
+++ src/components/md/Link.tsx
@@ -1,6 +1,8 @@
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 import { ComponentProps } from 'react'
 import styled from 'styled-components'
+import { isRelativeUrl, removeTrailingSlashes } from 'utils/text'
 
 const isExternalUrl = (url: string) => url.substr(0, 4) === 'http' || url.substr(0, 2) === '//'
 
@@ -13,8 +15,15 @@ const stripMdExtension = url => {
 }
 
 function Link({ href, children, ...props }: ComponentProps<typeof NextLink>) {
+  const router = useRouter()
+
+  href = stripMdExtension(href)
+  if (isRelativeUrl(href)) {
+    href = `${removeTrailingSlashes(router.pathname)}/${href}`
+  }
+
   return (
-    <NextLink href={stripMdExtension(href)}>
+    <NextLink href={href}>
       <a {...props}>{children}</a>
     </NextLink>
   )
