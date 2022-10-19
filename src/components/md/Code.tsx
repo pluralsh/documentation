8d4499a09d5bb462c2e6cb0ddb125e99cc4aa54a
Klink <85062+dogmar@users.noreply.github.com>
2022-10-19 12:04:00 -0700

    Use InlineCode component from design system

--- src/components/md/Code.tsx
+++ src/components/md/Code.tsx
@@ -1,30 +1,8 @@
-import styled from 'styled-components'
+import { InlineCode } from 'pluralsh-design-system'
 
 function Code({ ...props }) {
-  return <code {...props} />
+  return <InlineCode {...props} />
 }
 
-export default styled(Code)(({ theme }) => ({
-  fontFamily: theme.fontFamilies.mono,
-  fontSize: 14,
-  border: theme.borders.default,
-  borderRadius: theme.borderRadiuses.large,
-  paddingRight: theme.spacing.xxsmall,
-  paddingLeft: theme.spacing.xxsmall,
-  paddingTop: '1px',
-  paddingBottom: '2px',
-  color: theme.colors['text-light'],
-  backgroundColor: theme.colors['fill-one'],
-  '* ~ &': {
-    marginLeft: theme.spacing.xxsmall,
-  },
-  'a:any-link &': {
-    color: theme.colors['action-link-inline'],
-  },
-  'a:hover &': {
-    color: theme.colors['action-link-inline-hover'],
-  },
-  'a:visited &, a:active &': {
-    color: theme.colors['action-link-inline-visited'],
-  },
-}))
+export default Code
+
