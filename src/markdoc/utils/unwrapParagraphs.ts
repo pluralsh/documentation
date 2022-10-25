2f0782b3ff01501df2d61ecd3f2c02ef626c934f
Klink <85062+dogmar@users.noreply.github.com>
2022-10-25 13:55:53 -0700

    Fix issue with image captions

--- src/markdoc/utils/unwrapParagraphs.ts
+++ src/markdoc/utils/unwrapParagraphs.ts
@@ -1,11 +1,17 @@
 const unwrapParagraph = child => {
-  if (child.name === 'Paragraph') {
-    return child.children
+  if (child?.name === 'Paragraph') {
+    return child?.children
   }
 
   return child
 }
 
-const unwrapParagraphs = children => children.map(unwrapParagraph).flat()
+const unwrapParagraphs = children => {
+  if (!Array.isArray(children)) {
+    return unwrapParagraph(children)
+  }
+
+  return children.map(unwrapParagraph).flat()
+}
 
 export default unwrapParagraphs
