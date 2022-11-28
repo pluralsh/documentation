106dcdc5572e96710eb3bc9f8e499675c1c575c0
Klink <85062+dogmar@users.noreply.github.com>
2022-11-28 10:03:53 -0800

    Run graphql-codegen

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -1238,10 +1238,11 @@ export type InvoiceItem = {
 
 export type KeyBackup = {
   __typename?: 'KeyBackup';
+  digest: Scalars['String'];
   id: Scalars['ID'];
   insertedAt?: Maybe<Scalars['DateTime']>;
   name: Scalars['String'];
-  repositories?: Maybe<Array<Maybe<Scalars['String']>>>;
+  repositories?: Maybe<Array<Scalars['String']>>;
   updatedAt?: Maybe<Scalars['DateTime']>;
   user: User;
   value: Scalars['String'];
