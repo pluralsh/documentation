b2618af23f208b6e2448e155984b47abbcbe957d
Klink <85062+dogmar@users.noreply.github.com>
2022-11-30 12:38:17 -0800

    Remove test code

--- src/markdoc/nodes/fence.markdoc.ts
+++ src/markdoc/nodes/fence.markdoc.ts
@@ -6,17 +6,7 @@ export const fence = {
     content: { type: String, render: true, required: true },
     language: { type: String },
     process: { type: Boolean, render: true, default: false },
-    showHeader: {
-      type: Boolean,
-      required: false,
-      render: true,
-    },
+    showHeader: { type: Boolean, required: false, render: true },
     title: { type: String, required: false },
-    validate: (node, arg2) => {
-      console.log('node', node)
-      console.log('arg2', arg2)
-
-      return [{ id: 'stuff', level: 'critical', message: 'Hey, its a message' }]
-    },
   },
 }
