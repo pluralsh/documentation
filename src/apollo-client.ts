a05f4cdc34343f8616129de0b5c44cea2bc88fb1
Klink <85062+dogmar@users.noreply.github.com>
2022-11-30 12:39:22 -0800

    Remove no longer necessary auth-token for api access

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,11 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const token = process.env.GRAPHQL_TOKEN
-
 const client = new ApolloClient({
   uri: 'https://app.plural.sh/gql',
   cache: new InMemoryCache(),
-  headers: { authorization: token ? `Bearer ${token}` : '' },
 })
 
 export default client
