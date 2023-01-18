edb27b68bba9869844e39c05606ac00783109394
Klink <85062+dogmar@users.noreply.github.com>
2023-01-18 12:03:00 -0800

    Replace hotjar with posthog

--- src/components/ExternalScripts.tsx
+++ src/components/ExternalScripts.tsx
@@ -3,29 +3,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Script from 'next/script'
 
-const hotJarScript = `
-(function(h,o,t,j,a,r){
-    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:3188593,hjsv:6};
-    a=o.getElementsByTagName('head')[0];
-    r=o.createElement('script');r.async=1;
-    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-    a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-`
-
-function HotJar() {
-  return (
-    <>
-      {/* <!-- Hotjar Tracking Code for https://docs.plural.sh/ --> */}
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: hotJarScript }}
-      />
-    </>
-  )
-}
-
 function HubSpot() {
   const router = useRouter()
 
@@ -61,10 +38,26 @@ function HubSpot() {
   )
 }
 
+const posthogScript = `
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+posthog.init('phc_r0v4jbKz8Rr27mfqgO15AN5BMuuvnU8hCFedd6zpSDy',{api_host:'https://posthog.plural.sh'})
+`
+
+function Posthog() {
+  return (
+    <>
+      {/* <!-- Posthog Tracking Code for https://docs.plural.sh/ --> */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: posthogScript }}
+      />
+    </>
+  )
+}
 export default function ExternalScripts() {
   return (
     <>
-      <HotJar />
+      <Posthog />
       <HubSpot />
     </>
   )
