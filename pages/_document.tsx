d5c861264a7f73b7c2685a2add1b49e0dbc82bc5
Klink <85062+dogmar@users.noreply.github.com>
2023-03-02 12:27:00 -0800

    adjust gtag event order

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,10 +58,10 @@ class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 gtag('consent', 'default', {ad_storage:'denied', analytics_storage:'denied'});
 gtag('set', 'ads_data_redaction', true);
 gtag('set', 'url_passthrough', true);
+gtag('js', new Date());
 gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
           }}
           />
