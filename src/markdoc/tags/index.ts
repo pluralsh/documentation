57d6d534b187f3ab34aed0f5f57b001609ba71cf
Klink <85062+dogmar@users.noreply.github.com>
2023-04-20 10:55:04 -0700

    Fix incorrect import path

--- src/markdoc/tags/index.ts
+++ src/markdoc/tags/index.ts
@@ -1,5 +1,5 @@
 /* Markdoc tags must be exported from this file to work with markdoc/nextjs plugin */
 /* Use this file to export your markdoc tags */
 
-export * from '@pluralsh/design-system/dist/markdoc/schema/tags'
+export * from '@pluralsh/design-system/dist/markdoc/tags'
 export { comment, head, script, link } from './nextjs.markdoc'
