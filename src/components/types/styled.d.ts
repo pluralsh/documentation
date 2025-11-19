480b3ce9cad1a8005c539ca9cabcf4a8d2b885e9
Jake Laderman <jsladerman@gmail.com>
2025-11-19 17:34:02 -0500

    add mermaid support (#537)

--- src/components/types/styled.d.ts
+++ src/components/types/styled.d.ts
@@ -3,10 +3,21 @@ import 'styled-components'
 
 import { type styledTheme } from '@pluralsh/design-system'
 
+// allow css prop on html elements
+// also allow any CSS custom variable to be used in CSSProperties
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | undefined
+  }
+  interface CSSProperties {
+    [key: `--${string}`]: Nullable<string | number>
+  }
+}
+
 type StyledTheme = typeof styledTheme
 
-// and extend them!
+// extend original module declarations
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends StyledTheme {}
+  export declare function useTheme(): DefaultTheme
 }
