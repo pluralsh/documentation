7494182cef2df1af27994248aa4906e2d5f051a4
Jake Laderman <jsladerman@gmail.com>
2026-01-14 11:25:02 -0500

    add security headers

--- next.config.js
+++ next.config.js
@@ -34,6 +34,19 @@ const nextConfig = {
   async redirects() {
     return redirects
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'none'" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
