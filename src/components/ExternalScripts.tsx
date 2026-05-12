9cacf869cfedb44a344a725e04c6193cbae900ab
michaeljguarino <mjg@plural.sh>
2026-05-12 19:35:57 -0400

    0;276;0cSetup kapa.ai docs search
    Should be much better than what algolia provides, I hope

--- src/components/ExternalScripts.tsx
+++ src/components/ExternalScripts.tsx
@@ -60,6 +60,63 @@ function HubSpot() {
   )
 }
 
+// function safeKapaUnmount() {
+//   const kapa = window.Kapa
+
+//   if (typeof kapa !== 'function') {
+//     return
+//   }
+
+//   try {
+//     kapa('unmount')
+//   } catch {
+//     // Widget may not have mounted yet
+//   }
+// }
+
+function KapaWidget() {
+  // useEffect(() => {
+  //   const onCookiePrefChange = () => {
+  //     if (!window.Cookiebot?.consent?.statistics) {
+  //       safeKapaUnmount()
+  //     }
+  //   }
+
+  //   window.addEventListener('CookiebotOnAccept', onCookiePrefChange)
+  //   window.addEventListener('CookiebotOnDecline', onCookiePrefChange)
+
+  //   return () => {
+  //     window.removeEventListener('CookiebotOnAccept', onCookiePrefChange)
+  //     window.removeEventListener('CookiebotOnDecline', onCookiePrefChange)
+  //   }
+  // }, [])
+
+  return (
+    <Script
+      // id="kapa-widget-script"
+      // type="text/plain"
+      // data-cookieconsent="statistics"
+      strategy="afterInteractive"
+      async
+      defer
+      src="https://widget.kapa.ai/kapa-widget.bundle.js"
+      data-website-id="8288b978-8aca-4409-9cbd-de12c446cf07"
+      data-project-name="Plural"
+      data-view-mode="sidebar"
+      data-modal-open-on-command-k="true"
+      data-search-mode-enabled="true"
+      data-mcp-enabled="true"
+      data-project-color="#5D63F4"
+      data-project-logo="https://docs.plural.sh/favicon-192.png"
+    />
+  )
+}
+
 export default function ExternalScripts() {
-  return <HubSpot />
+  return (
+    <>
+      <HubSpot />
+      <KapaWidget />
+    </>
+  )
 }
